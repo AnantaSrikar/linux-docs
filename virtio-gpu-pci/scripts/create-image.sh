13d2f2ba9c989de21ab080c6e49a2b9a37ee208a
AnantaSrikar <anantasrikar@pm.me>
2026-07-08 15:18:06 -0400

    virtio-gpu-pci and vkms: improved kernel config

--- virtio-gpu-pci/scripts/create-image.sh
+++ virtio-gpu-pci/scripts/create-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ADDITIONAL_PACKAGES="make,git,vim,tmux,build-essential,cmake,libdrm-dev,libkmod-dev,libproc2-dev,libdw-dev,libpixman-1-dev,libcairo2-dev,libudev-dev,flex,bison,pciutils,software-properties-common,pkg-config"
+ADDITIONAL_PACKAGES="make,git,vim,tmux,build-essential,cmake,libdrm-dev,libkmod-dev,libproc2-dev,libdw-dev,libpixman-1-dev,libcairo2-dev,libudev-dev,flex,bison,pciutils,software-properties-common,pkg-config,libdrm-tests"
 
 mkdir image
 
